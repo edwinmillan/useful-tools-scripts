1533ca11ddc98288b30a94abc99dc5e9b7bc46c3
Edwin Millan <bassgs3000@gmail.com>
2017-07-10 14:40:39 -0500

    Upload Basescript
    This Script handles a server list and can perform actions upon them if PSRemote is properly setup.

---
+++ Basescript.ps1
@@ -0,0 +1,28 @@
+﻿Function Get-FileName
+{
+    # Function came from this website: http://www.workingsysadmin.com/open-file-dialog-box-in-powershell/
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = Get-Location
+    #$OpenFileDialog.filter = "Text Document (*.txt)| *.txt"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+# Make a text file that contains your server names each in a line of its own. 
+
+$inputfile = Get-FileName
+$serverlist = (Get-Content $inputfile)
+
+$credentials = Get-Credential
+
+ForEach($server in $serverlist) {
+    
+    #$servername = "$server.$DomainName"
+    
+    #Write-Host "################## $servername ##################"
+    
+    # Uncomment and fill in the scriptblock with a custom command, here a simple google ping has been set up.
+    #Invoke-Command -ComputerName $servername -Credential $credentials -ScriptBlock {ping google.com}
+}
